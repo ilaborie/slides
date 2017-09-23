df47b13bc51bd5a03e6a5778428e604acb1c4051
Igor Laborie <ilaborie@gmail.com>
2017-09-23 18:14:17 +0200

    wip

--- src/main/kotlin/org/ilaborie/slides/content/ext-html.kt
+++ src/main/kotlin/org/ilaborie/slides/content/ext-html.kt
@@ -42,7 +42,7 @@ fun Presentation.renderAsHtml(key: String): String {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>${title.renderAsString()}</title>
     <link rel="stylesheet" href="slides.css">
